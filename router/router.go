796c9c36786aab1411b76ce35051116fbfa4017b
SA <mxsadegh@gmail.com>
2020-08-23 03:03:14 +0430

    main hcnaged to server

--- router/router.go
+++ router/router.go
@@ -1,17 +1,24 @@
 package router
 
 import (
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/gorilla/sessions"
 	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/xaoirse/logbook/controller"
+	"github.com/xaoirse/logbook/graph"
+	"github.com/xaoirse/logbook/graph/generated"
 	mymidd "github.com/xaoirse/logbook/middleware"
 )
 
 // New return a new *Echo
 func New(db *gorm.DB) *echo.Echo {
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
 	e := echo.New()
 	// TODO random secret generator
 	// Note: Don't store your key in your source code. Pass it via an
@@ -23,12 +30,17 @@ func New(db *gorm.DB) *echo.Echo {
 	e.Use(middleware.Secure())
 	// TODO uncomment for release or write better one
 	// e.Use(middleware.Logger())
+
 	// TODO a middleware for flood check
 	// Root
-	e.GET("/", controller.Index(db))
+	e.GET("/home/", controller.Index(db))
 	e.GET("/login/", controller.LoginPage(db))
 	e.POST("/login/", controller.Login(db))
 	e.POST("/logout/", controller.Logout(db))
+
+	// gqlgen
+	e.GET("/", echo.WrapHandler(playground.Handler("GraphQL playground", "/query")))
+	e.GET("/query/", echo.WrapHandler(srv))
 
 	// Dashboard
 	dash := e.Group("/dashboard")
--- server.go
+++ server.go
@@ -1,29 +1,57 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"log"
-	"net/http"
 	"os"
 
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/xaoirse/logbook/graph"
-	"github.com/xaoirse/logbook/graph/generated"
+	"github.com/labstack/echo/v4"
+	"github.com/xaoirse/logbook/model"
+	"github.com/xaoirse/logbook/router"
 )
 
-const defaultPort = "8080"
+// func main() {
+
+// 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+// 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+// 	http.Handle("/query", srv)
+
+// 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+// 	log.Fatal(http.ListenAndServe(":"+port, nil))
+// }
+
+// Template is
+type Template struct {
+	templates *template.Template
+}
+
+// Render is
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+const defaultPort = "4000"
 
 func main() {
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	db := model.GetDb()
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Fatalln("Error when closing db:", err)
+		}
+	}()
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	r := router.New(db)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	t := &Template{templates: template.Must(template.ParseGlob("template/*.html"))}
+	r.Renderer = t
+
+	r.Logger.Fatal(r.Start(":" + defaultPort))
 }
